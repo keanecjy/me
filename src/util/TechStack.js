344939da788812a9be1e3aecf5e6be29614ec4c1
keanecjy <e0425601@u.nus.edu>
2021-01-03 21:16:19 +0800

    Add tech stack file

---
+++ src/util/TechStack.js
@@ -0,0 +1,24 @@
+import react from '../images/react.png';
+import javascript from '../images/javascript.png';
+import firebase from '../images/firebase.png';
+import java from '../images/java.svg';
+import python from '../images/python.png';
+import css from '../images/css.png';
+import html from '../images/html.png';
+import adobexd from '../images/adobexd.png';
+import javafx from '../images/JavaFX_Logo.png';
+import nodejs from '../images/nodejs-icon.svg';
+import c from '../images/C.png';
+
+export const reactIcon = {icon: react, name: 'React'};
+export const jsIcon = {icon: javascript, name: 'JavaScript'};
+export const firebaseIcon = {icon: firebase, name: 'Firebase'};
+export const javaIcon = {icon: java, name: 'Java'};
+export const pythonIcon = {icon: python, name: 'Python'};
+export const cssIcon = {icon: css, name: 'CSS'};
+export const htmlIcon = {icon: html, name: 'HTML'};
+export const adobeXdIcon = {icon: adobexd, name: 'Adobe XD'};
+
+export const javaFxIcon = {icon: javafx, name: 'JavaFX'};
+export const nodeJsIcon = {icon: nodejs, name: 'NodeJS'};
+export const cIcon = {icon: c, name: 'C'};

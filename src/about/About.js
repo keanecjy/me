37cb1f8bcc935208c534b361db8c2d39a878e060
keanecjy <e0425601@u.nus.edu>
2021-01-03 21:15:58 +0800

    Add about section

---
+++ src/about/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './styles.css';
+
+const About = () => {
+  return (
+    <div id="#about" className="about">
+      <h1>About me</h1>
+      <p>
+        I enjoy learning and exploring concepts in the vast field of Computer Science. More recently,
+        I have been learning Front-end development and Artificial Intelligence.
+      </p>
+    </div>
+  );
+};
+
+export default About;

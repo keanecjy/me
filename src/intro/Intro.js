d86f71e2d0c4f0fcb8d17390f8a50656e1cfb7b6
keane <e0425601@u.nus.edu>
2021-05-06 23:54:11 +0800

    Update intro

--- src/intro/Intro.js
+++ src/intro/Intro.js
@@ -8,8 +8,8 @@ const Intro = () => {
   const INTRO =
     "Hi there! I'm Keane, and I enjoy building software that is both elegant and intuitive.";
   const NUS_LINK = 'http://nus.edu.sg/';
-  const YEAR_OF_STUDY = 'I am currently a year 2 Computer Science undergraduate at ';
-  const SCHOOL = 'National University of Singapore';
+  const YEAR_OF_STUDY = 'I am currently a Computer Science undergraduate at ';
+  const SCHOOL = 'National University of Singapore.';
 
   return (
     <IsVisible once>

7134906420a10c74c3dcb6cdd0915e44cbf74c88
keane <e0425601@u.nus.edu>
2021-06-12 18:12:54 +0800

    Updates

---
+++ serviceworker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "version-1";
+const urlsToCache = ["index.html"];
+
+const self = this;
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache)
+      })
+  )
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(() => {
+        return fetch(event.request)
+          .catch(() => caches.match('index.html'))
+      })
+  )
+})
+
+self.addEventListener('activate', (event) => {
+  const cacheWhiteList = [];
+  cacheWhiteList.push(CACHE_NAME)
+
+  event.waitUntil(
+    caches.keys().then((cacheNames) => Promise.all(
+      // eslint-disable-next-line array-callback-return
+      cacheNames.map((cacheName) => {
+        if (!cacheWhiteList.includes(cacheName)) {
+          return caches.delete(cacheName);
+        }
+      })
+    ))
+  )
+})
